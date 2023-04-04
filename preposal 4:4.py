ea45c6e7ac1d5c7b9e4654e56c4c3f0459d4eadc
Rosselinne <129908929+Rosselinne@users.noreply.github.com>
2023-04-04 19:54:18 -0400

    Add files via upload

---
+++ preposal 4:4.py
@@ -0,0 +1,22 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+A home page that has my personal inforamtion, links and allow the user to browrse
+
+or
+
+A game such where the user can answer question based basic low grade bioology questions, this game should allow the user to answer but also get help/hint if needed 
+
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Saykon T and Assiatusow Kamara 
+
+
+
+## Do you have any questions of your own?
+N/a
+
+
